3b163c9ae9d620e8bded2b0ce17b62e52b9f2579
Abhishek Kumar <abhishekkr95.dev@gmail.com>
2025-06-16 19:32:00 +0530

    Added a new problems

---
+++ 5)LinkedList/14)MergeTwoSortedList.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+var mergeTwoLists = function (l1, l2) {
+    let preHead = new ListNode(-1);
+    let prev = preHead;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val <= l2.val) {
+            prev.next = l1;
+            l1 = l1.next;
+        } else {
+            prev.next = l2;
+            l2 = l2.next;
+        }
+        prev = prev.next;
+    }
+
+    prev.next = l1 === null ? l2 : l1;
+
+    return preHead.next;
+};
